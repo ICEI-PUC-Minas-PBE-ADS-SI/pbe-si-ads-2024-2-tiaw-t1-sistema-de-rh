baa0e459bd46deee594972394d502225ba0070cb
Lucas Souza <lucashsouza.343@gmail.com>
2024-10-27 23:28:06 -0300

    Create dbCandidatos.js

---
+++ codigo-fonte/pagina-cadastro-candidato/dbCandidatos.js
@@ -0,0 +1,7 @@
+// Banco de dados simulado de empresas
+let dbCandidatos = [];
+
+// Função para listar todas as empresas cadastradas (apenas para verificação)
+function listarCandidatos() {
+    console.log('Candidatos cadastradas:', dbCandidatos);
+}
